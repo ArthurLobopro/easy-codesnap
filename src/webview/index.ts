02d68dd91343f1a5769487acf874545dfa4ccb0a
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2024-01-23 15:55:39 -0300

    refactor: move actions to separate file

--- src/webview/index.ts
+++ src/webview/index.ts
@@ -1,11 +1,9 @@
-import { pick, pickAllExcept } from "@arthur-lobo/object-pick"
-import { ConfigSentToWebview } from "../types"
 import { ContentManager } from "./ContentManager"
-import { alreadyHasSessionConfig, getSessionConfig, setSessionConfig } from "./configManager"
+import { actions, actionsKey } from "./actions"
+import { getSessionConfig } from "./configManager"
 import { btnSave } from "./elements"
-import { cameraFlashAnimation, takeSnap } from "./snap"
+import { takeSnap } from "./snap"
 import { addListeners } from "./ui/listeners"
-import { UIUpdater, UpdateCode } from "./ui/updaters"
 import { vscode } from "./util"
 
 btnSave.addEventListener("click", () => takeSnap())
@@ -20,69 +18,15 @@ document.addEventListener("paste", (e) => {
     }
 })
 
-const actions = {
-    flash: cameraFlashAnimation,
-
-    update(config: ConfigSentToWebview) {
-        if (alreadyHasSessionConfig() && getSessionConfig().isLocked) {
-            return
-        }
-
-        setSessionConfig(pickAllExcept(config, ["linkOnOpen", "lockOnOpen"]))
-        document.execCommand("paste")
-
-        setSessionConfig({
-            isLinked: config.linkOnOpen,
-            isLocked: config.lockOnOpen
-        })
-
-        UIUpdater()
-    },
-
-    "update-text"(config: ConfigSentToWebview) {
-        if (!alreadyHasSessionConfig()) {
-            setSessionConfig(config)
-        } else {
-            const { isLocked, isLinked, editorID } = getSessionConfig()
-
-            if (isLocked || isLinked && editorID !== config.editorID) {
-                return
-            }
-
-            setSessionConfig(pick(config, ["windowTitle", "startLine", "editorID"]))
-        }
-        UIUpdater()
-        document.execCommand("paste")
-    },
-
-    "update-config"(config: ConfigSentToWebview) {
-        setSessionConfig(
-            pickAllExcept(
-                config,
-                [
-                    "startLine",
-                    "windowTitle",
-                    "editorID",
-                    "linkOnOpen",
-                    "lockOnOpen"
-                ]
-            )
-        )
-        UIUpdater()
-        UpdateCode()
-    }
-}
-
 window.addEventListener("message", ({ data: { type, ...config } }) => {
     if (type in actions) {
-        actions[type as keyof typeof actions](config)
+        actions[type as actionsKey](config)
     } else {
         console.log(`Unknow action on renderer: ${actions}`)
     }
 })
 
 window.addEventListener("DOMContentLoaded", () => {
+    addListeners()
     vscode.postMessage({ type: "ready" })
 }, { once: true })
-
-addListeners()
