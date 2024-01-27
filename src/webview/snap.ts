5d99faa2be5bb20af8c7f5251c7c7d47826bd226
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2024-01-26 22:13:02 -0300

    Update snap.ts

--- src/webview/snap.ts
+++ src/webview/snap.ts
@@ -60,7 +60,11 @@ async function exportPNG(target: HTMLElement, action: WebviewConfig["shutterActi
     if (action === "copy") {
         const binary = atob(data)
         const array = new Uint8Array(binary.length)
-        for (let i = 0; i < binary.length; i++) { array[i] = binary.charCodeAt(i) }
+
+        for (let i = 0; i < binary.length; i++) {
+            array[i] = binary.charCodeAt(i)
+        }
+
         const blob = new Blob([array], { type: "image/png" })
         navigator.clipboard.write([new ClipboardItem({ "image/png": blob })])
         cameraFlashAnimation()
