a05aba0ab958ab66d3633c06a015ecb3864566a0
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2024-02-17 12:00:01 -0300

    refactor: convert arrow function into named function

--- src/webview/util.ts
+++ src/webview/util.ts
@@ -17,22 +17,26 @@ export function $$<T extends HTMLElement>(
     return Array.from(c.querySelectorAll(q)) as T[];
 }
 
-export const once = (elem: Element, evt: string) =>
-    new Promise((done) => elem.addEventListener(evt, done, { once: true }));
+export function once(elem: Element, evt: string) {
+    return new Promise((done) =>
+        elem.addEventListener(evt, done, { once: true }),
+    );
+}
 
 export const redraw = (node: Element) => node.clientHeight;
 
-export const setVar = (key: string, value: string, node = document.body) =>
-    node.style.setProperty("--" + key, value);
+export function setVar(key: string, value: string, node = document.body) {
+    return node.style.setProperty("--" + key, value);
+}
 
-export const calcTextWidth = (text: string) => {
+export function calcTextWidth(text: string) {
     const div = document.body.appendChild(document.createElement("div"));
     div.classList.add("size-test");
     div.textContent = text;
     const width = div.clientWidth;
     div.remove();
     return width + 1 + "px";
-};
+}
 
 declare const acquireVsCodeApi: () => {
     postMessage: (message: message) => void;
