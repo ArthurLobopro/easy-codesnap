1a2fad2b7d6cc99b438cfffca557c36a378a7abc
CoffeeChaton <63182963+CoffeeChaton@users.noreply.github.com>
2024-06-21 13:50:31 +0800

    fix: win case

--- src/commands/snap/savers.ts
+++ src/commands/snap/savers.ts
@@ -1,10 +1,13 @@
-import { homedir } from "os";
+import { homedir , platform} from "os";
 import path from "path";
 import * as vscode from "vscode";
 import { reduceSVG } from "../../reduceSVG";
 import { writeFile } from "../../util";
 
-const makeUri = vscode.Uri.parse;
+const isWin = (platform() === 'win32');
+const makeUri = isWin
+    ?vscode.Uri.file;
+    :vscode.Uri.parse;
 const uriPath = (uri: vscode.Uri) => uri.fsPath;
 
 let lastUsedImageUri = makeUri(path.resolve(homedir(), "Desktop"));
