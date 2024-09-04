ff3b014157e13754fe8f5d942185dfac38001573
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2024-09-04 10:48:49 -0300

    fix: update regex

--- src/webview/ui/updaters/TranslationUpdater.ts
+++ src/webview/ui/updaters/TranslationUpdater.ts
@@ -3,7 +3,7 @@ import { $$, t } from "../../util";
 export function TranslationUpdater() {
     $$("[data-l10n]").forEach((el) => {
         el.innerHTML = t(el.dataset.l10n as string).replace(
-            /`(\S.{1,}\S)`/g,
+            /`(\S[^`]{1,}\S)`/g,
             (str) => {
                 const content = str.substring(1, str.length - 1);
                 return `<code>${content}</code>`;
