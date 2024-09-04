a8c1b5cca82d227bc437e556c02b7d0894352dc6
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2024-09-04 09:12:36 -0300

    feat: add code markdown sintax

--- src/webview/ui/updaters/TranslationUpdater.ts
+++ src/webview/ui/updaters/TranslationUpdater.ts
@@ -2,6 +2,12 @@ import { $$, t } from "../../util";
 
 export function TranslationUpdater() {
     $$("[data-l10n]").forEach((el) => {
-        el.innerHTML = t(el.dataset.l10n as string);
+        el.innerHTML = t(el.dataset.l10n as string).replace(
+            /`(\S.{1,}\S)`/g,
+            (str) => {
+                const content = str.substring(1, str.length - 1);
+                return `<code>${content}</code>`;
+            },
+        );
     });
 }
