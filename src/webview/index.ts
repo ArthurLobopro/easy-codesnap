5fd0066ff9ef1c064146822f96a118851c941a52
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2025-05-07 07:11:46 -0300

    perf: translate a single time to improve performance

--- src/webview/index.ts
+++ src/webview/index.ts
@@ -5,7 +5,6 @@ import { actions, type actionsKey } from "./actions";
 import { takeSnap } from "./snap";
 import { btnSave } from "./ui/elements";
 import { addListeners } from "./ui/listeners";
-import { TranslationUpdater } from "./ui/updaters/TranslationUpdater";
 import { vscode } from "./util";
 
 registerLoadSVG();
@@ -35,7 +34,6 @@ window.addEventListener(
     "DOMContentLoaded",
     () => {
         addListeners();
-        TranslationUpdater();
         vscode.postMessage({ type: "ready" });
     },
     { once: true },
