9c21c151b4aa534f46094d5cc0e77b648aad620a
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2025-05-11 10:47:54 -0300

    Update compactJson.sh

--- scripts/compactJson.sh
+++ scripts/compactJson.sh
@@ -3,8 +3,7 @@
 ROOT="$PWD"
 
 # Encontra todos os arquivos .json a partir do diretório atual (inclusive em l10n)
-find "$ROOT" -type f -name "*.json" | while read -r file; do
-  tmp="$(mktemp)"
+find "$ROOT" -type d -name node_modules -prune -false -o -type f -name "*.json" | while read -r file; do  tmp="$(mktemp)"
   if jq -c . "$file" > "$tmp"; then
     mv "$tmp" "$file"
     echo "✅ Compressed: $file"
