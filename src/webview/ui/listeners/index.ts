a6e03c582932b54dd0c21e0f287a4f7e90e57679
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2025-07-13 01:08:38 -0300

    refactor: remove unused listener

--- src/webview/ui/listeners/index.ts
+++ src/webview/ui/listeners/index.ts
@@ -2,12 +2,9 @@ import { SessionConfig } from "../../SessionConfig";
 import { snippetContainerNode, windowNode } from "../elements";
 import { UpdateRatio, UpdateTargetProportion } from "../updaters";
 import { addContentEditableListeners } from "./addContentEditableListeners";
-import { addOpenConfigListeners } from "./addOpenConfigListeners";
 
 export function addListeners() {
   addContentEditableListeners();
-  // addHideOneTimeConfigListener();
-  addOpenConfigListeners();
 
   const resizeObserver = new ResizeObserver(() => {
     if (SessionConfig.get("aspectRatio") !== "none") {
