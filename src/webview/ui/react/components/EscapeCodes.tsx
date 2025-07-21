5c423869d6f1f2b121769cf8640d364ec50dadcf
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2025-07-21 09:38:01 -0300

    fix: regex to escape codes

--- src/webview/ui/react/components/EscapeCodes.tsx
+++ src/webview/ui/react/components/EscapeCodes.tsx
@@ -1,8 +1,8 @@
 /** biome-ignore-all lint/suspicious/noArrayIndexKey: key need to be index */
 import { memo } from "react";
 
-const codeRegex = /(`\S[^`]{1,}\S`)/;
-const codeContentRegex = /`(\S[^`]{1,}\S)`/;
+const codeRegex = /(`\S[^`]+\S`)/;
+const codeContentRegex = /`(\S[^`]+\S)`/;
 
 export const EscapeCodes = memo(function EscapeCodes({ text }: { text: string }) {
   return text.split(codeRegex).map((part, i) => {
