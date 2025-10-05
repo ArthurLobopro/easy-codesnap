d1d55c486bb6f2115085990c72f90854c2387c2b
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2025-10-05 13:08:28 -0300

    fix: sort order

--- cli/commands/sort.order.ts
+++ cli/commands/sort.order.ts
@@ -1,11 +1,10 @@
 import { readFileSync, writeFileSync } from "node:fs";
-import { resolve } from "node:path";
 import { omit } from "@arthur-lobo/object-pick";
 import type { contributes } from "../../package.json";
-import { root } from "../constants";
+import { packagePath } from "../constants";
 
 export function SortOrder() {
-  const packageContent = JSON.parse(readFileSync(resolve(root, "./package.json"), "utf-8"));
+  const packageContent = JSON.parse(readFileSync(packagePath, "utf-8"));
 
   const properties = packageContent.contributes.configuration
     .properties as (typeof contributes)["configuration"]["properties"];
@@ -19,5 +18,5 @@ export function SortOrder() {
     };
   });
 
-  writeFileSync(resolve(__dirname, "../package.json"), JSON.stringify(packageContent, null, 2));
+  writeFileSync(packagePath, JSON.stringify(packageContent, null, 2));
 }
--- cli/constants.ts
+++ cli/constants.ts
@@ -2,6 +2,8 @@ import { readdirSync } from "node:fs";
 import { resolve } from "node:path";
 
 export const root = resolve(__dirname, "..");
+export const packagePath = resolve(root, "package.json");
+
 export const nls_json_files = readdirSync(root, { withFileTypes: true })
   .filter((f) => f.isFile() && /(package.nls.([\w-]+).json)$/g.test(f.name))
   .map((f) => resolve(f.parentPath, f.name));
