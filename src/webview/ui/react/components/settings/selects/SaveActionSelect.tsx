902ae37c9667c715193671249667917b6ac4ae94
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2026-01-03 23:13:19 -0300

    i18n: new translation

--- src/webview/ui/react/components/settings/selects/SaveActionSelect.tsx
+++ src/webview/ui/react/components/settings/selects/SaveActionSelect.tsx
@@ -1,8 +1,11 @@
+import { TooltipText } from "@arthur-lobo/react-custom-tooltip";
 import { useConfigList, useSetConfig } from "@hooks//useConfig";
 import { useTranslation } from "@hooks//useTranslation";
 import type { SaveAction } from "@/../types";
 import { SAVE_ACTIONS } from "@/constants";
 import { ConfigSelect } from "../../ConfigSelect";
+import { EscapeCodes } from "../../EscapeCodes";
+import { LeftTooltip } from "../../LeftTooltip";
 import { SettingLineWithSelect } from "../../SettingLine";
 
 export function SaveActionSelect() {
@@ -12,23 +15,32 @@ export function SaveActionSelect() {
   const { shutterAction: saveAction, saveFormat } = useConfigList(["shutterAction", "saveFormat"]);
 
   return (
-    <SettingLineWithSelect>
-      <span>{t("Save Action")}</span>
-      <ConfigSelect
-        tabIndex={-1}
-        value={saveAction}
-        onChange={(ev) => {
-          set({
-            shutterAction: ev.currentTarget.value as SaveAction,
-          });
-        }}
-      >
-        {SAVE_ACTIONS.map((action) => (
-          <option value={action} key={action} disabled={action === "copy" && saveFormat === "webp"}>
-            {action}
-          </option>
-        ))}
-      </ConfigSelect>
-    </SettingLineWithSelect>
+    <LeftTooltip>
+      <SettingLineWithSelect>
+        <span>{t("Save Action")}</span>
+        <ConfigSelect
+          tabIndex={-1}
+          value={saveAction}
+          onChange={(ev) => {
+            set({
+              shutterAction: ev.currentTarget.value as SaveAction,
+            });
+          }}
+        >
+          {SAVE_ACTIONS.map((action) => (
+            <option value={action} key={action} disabled={action === "copy" && saveFormat === "webp"}>
+              {action}
+            </option>
+          ))}
+        </ConfigSelect>
+      </SettingLineWithSelect>
+      <TooltipText>
+        <EscapeCodes
+          text={t("The option `copy` is not suported when `{saveFormat}` is set to `webp`", {
+            saveFormat: t("Save Format"),
+          })}
+        />
+      </TooltipText>
+    </LeftTooltip>
   );
 }
